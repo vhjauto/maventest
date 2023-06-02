59d0575744287ebe4d9320f21c7306b67e64ac82
vhjauto <134847015+vhjauto@users.noreply.github.com>
2023-06-02 11:37:19 +0530

    Update LoginPage.java

--- src/main/java/Pages/LoginPage.java
+++ src/main/java/Pages/LoginPage.java
@@ -1,5 +1,11 @@
 package Pages;
 
 public class LoginPage {
+  
+  public void enterusername(String un){
+    
+    driver.findElement(By.id("name")).sendKeys(un);
+    Thread.sleep(2000);
+  }
 
 }
